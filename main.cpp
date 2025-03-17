e53d659f6d0407bd4ed52172573f231bce14dd4d
Karen Alvarez <kalva088@ucr.edu>
2025-03-17 00:46:42 -0700

    s

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <vector>
 #include <string>
 
 using namespace std;
 
-void forwardSearch(ifstream &file){
+void forwardSearch(){
     cout<< "Running Forward Search"<<endl;
 
 
@@ -14,27 +16,41 @@ void forwardSearch(ifstream &file){
 
 
 
-int main(int argc, char* argv[]){
-    if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " <filename>";
-        return 1;
-    }
-    ifstream file(argv[1]);
-    if (!file) {
-        cerr << "Error: Could not open file " << argv[1] << endl;
-        return 1;
-    }
-
+int main(){
     cout<< "Feature Selection with Nearest Neighbor"<<endl;
     cout<< "Please choose which search you would like to try:"<<endl;
     cout<< "1. Forward Search"<<endl<< "2.Backwards Search"<<endl;
-    int userInput=0;
+    int userInput;
     cin>> userInput;
     cout<<endl;
 
+    cout<< "Please choose which data set you would like to use:"<<endl;
+    cout<< "1. Small Dataset"<<endl<< "2. Large Dataset"<<endl;
+    int dtset;
+    cin>> dtset;
+    cout<<endl;
+
+    string filename;
+    if(dtset==1){
+        filename="CS170_Small_Data__74.txt";
+    } 
+    else if(dtset==2){
+        filename="CS170_Large_Data__119.txt";
+    } 
+    else{
+        cout<< "Invalid dataset choice"<<endl;
+        return 0;
+    }
+
+    ifstream file(filename);
+    if(!file){
+        cerr<< "Error: can't open file "<< filename<<endl;
+        return 1;
+    }
+
     if(userInput==1){
         //FORWARD SEARCH
-        forwardSearch(file);
+        forwardSearch();
     }else if( userInput ==2){
         //BACKWARDS SEARCH
         
@@ -43,6 +59,5 @@ int main(int argc, char* argv[]){
         return 0;
     }
     
-    file.close();
     return 0;
 }
