77a813a1cbf540ff5b45e0f9c83ae2fe64271d2a
Karen Alvarez <kalva088@ucr.edu>
2025-03-16 23:52:54 -0700

    main intro

--- main.cpp
+++ main.cpp
@@ -3,5 +3,22 @@
 using namespace std;
 
 int main(){
+    cout<< "Feature Selection with Nearest Neighbor"<<endl;
+    cout<< "Please choose which search you would like to try:"<<endl;
+    cout<< "1. Forward Search"<<endl<< "2.Backwards Search"<<endl;
+    int userInput=0;
+    cin>> userInput;
+
+    if(userInput==1){
+        //FORWARD SEARCH
+        cout<< "1. Forward Search"<<endl;
+    }else if( userInput ==2){
+        //BACKWARDS SEARCH
+        cout<< "1. Backwards Search"<<endl;
+    }else{
+        cout<< "Incorrect input"<<endl;
+        return 0;
+    }
     
+    return 0;
 }
